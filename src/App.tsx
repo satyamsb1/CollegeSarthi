f886e33efef54a43c47a3c1404360cce702c19ff
Vineet Jadhav <vineet290403@gmail.com>
2024-04-21 00:46:14 +0530

    Added Authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import "./App.css";
 import KanbanBoard from "./components/KanbanBoard";
-import Navbar from "../src/components/Navbar";
+// import Navbar from "../src/components/Navbar";
 import Login from "./components/Login";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <KanbanBoard />;{/* <Login /> */}
-    </Router>
+
+    <BrowserRouter>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<KanbanBoard />} />
+      </Routes>
+    </BrowserRouter>
+
+
   );
 }
 
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,17 +1,44 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
+import baseURL from "../apis/apiConfig";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Login() {
-  const [email, setEmail] = useState("");
+  const [staffId, setStaffId] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
-    // Here you can implement your login logic
-    console.log("Login clicked with email:", email, "and password:", password);
+
+    try {
+      const response = await axios.post(`${baseURL}/api/auth/staff_login`, {
+        staff_id: staffId,
+        password: password,
+      });
+
+      if (response.status === 200) {
+        const data = response.data;
+        localStorage.setItem("token", data.access_token);
+        navigate("/dashboard");
+
+      } else {
+        console.error("Login failed");
+        toast.error("Incorrect staff ID or password. Please try again.", { theme: "dark" });
+      }
+    }
+    catch (error) {
+      console.error("Error:", error);
+      toast.error("An error occurred. Please try again later.");
+    }
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen ">
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <ToastContainer theme="dark" />
       <form
         onSubmit={handleSubmit}
         className="bg-[#f7f8f9] p-8 rounded-lg shadow-lg">
@@ -25,8 +52,8 @@ function Login() {
           <input
             type="staffID"
             id="staffID"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={staffId}
+            onChange={(e) => setStaffId(e.target.value)}
             placeholder="Enter your Staff ID"
             className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-[#0c66e4] focus:border-[2px]"
             required
